b61e5257b5498bf297ea856ab95470e0c5c4df41
Gabyvargut <150144539+Gabyvargut@users.noreply.github.com>
2023-12-04 12:16:58 +0100

    Add files via upload

---
+++ analizar_insulina_con_python.py
@@ -0,0 +1,49 @@
+#python3.6 // Cod Lab 120
+
+#Exercise 1: Assigning variables to the sequence elements of human insulin
+## Store the human preproinsulin sequence in a variable called preproinsulin:
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktr" \
+"reaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+
+## Store the remaining sequence elements of human insulin in variables:
+lsInsulin = "malwmrllpllallalwgpdpaaa"
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+aInsulin = "giveqcctsicslyqlenycn"
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
+
+## Merge the results of the smaller insulin groupings into a single variable called insulin:
+insulin = bInsulin + aInsulin
+
+#Exercise 3: Using print() to display sequences of human insulin to the console
+## Printing "the sequence of human insulin" to console using successive print() commands:
+print("The sequence of human preproinsulin:")
+
+## Print a string that is contained in a variable from your script
+print(preproInsulin)
+
+## Printing to console using concatenated strings inside the print function (one-liner):
+print("The sequence of human insulin, chain a: " + aInsulin)
+
+#Exercise 4: Calculating the rough molecular weight of human insulin using the given code
+# Calculating the molecular weight of insulin  
+# Creating a list of the amino acid (AA) weights  
+aaWeights = {'A': 89.09, 'C': 121.16, 'D': 133.10, 'E': 147.13, 'F': 165.19,
+'G': 75.07, 'H': 155.16, 'I': 131.17, 'K': 146.19, 'L': 131.17, 'M': 149.21,
+'N': 132.12, 'P': 115.13, 'Q': 146.15, 'R': 174.20, 'S': 105.09, 'T': 119.12,
+'V': 117.15, 'W': 204.23, 'Y': 181.19}  
+# Count the number of each amino acids  
+aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A', 'C',
+'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+'V', 'W', 'Y']})  
+# Multiply the count by the weights  
+molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
+['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R',
+'S', 'T', 'V', 'W', 'Y']}.values())  
+print("The rough molecular weight of insulin: " +
+str(molecularWeightInsulin))
+
+#The actual molecular weight of human insulin is 5807.63, but the program delivers 6696.42
+#because it ignores certain bonds and post-translational processing.
+molecularWeightInsulinActual = 5807.63
+print("Error percentage: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
+
